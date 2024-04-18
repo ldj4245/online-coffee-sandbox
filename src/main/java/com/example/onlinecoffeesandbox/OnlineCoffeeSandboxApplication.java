d8d23e217ed92bb60d8ff7464bba0448f666c688
LEEDAEJIN <ldj4241@naver.com>
2024-04-18 10:41:59 +0900

    Initial commit

---
+++ src/main/java/com/example/onlinecoffeesandbox/OnlineCoffeeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinecoffeesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCoffeeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineCoffeeSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlinecoffeesandbox/OnlineCoffeeSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinecoffeesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCoffeeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
